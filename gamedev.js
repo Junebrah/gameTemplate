153ee4a6fc4596bc0cfa06c18c4ba82baf814427
Junebrah <200808@hkis.edu.hk>
2015-12-14 12:38:30 +0800

    fixing stuff

--- gamedev.js
+++ gamedev.js
@@ -23,6 +23,8 @@ $( document ).ready(function() {
 		$("#exploreBody").css("background-image", "url('img/cave5.jpg')");
 		$("#goStraight4").show();
 		$("#goLeft2").show();
+		$("#goRight2").hide();
+		$("#goStraight1").hide();
 	});
 
 	$("#goStraight4").click(function() {
